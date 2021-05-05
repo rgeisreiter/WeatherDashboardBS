41c0ae1b2e4e4f6cf7df73d45ec1a389e58b9eb3
rgeisreiter <rgeisreiter@yahoo.com>
2021-05-05 15:56:45 -0600

    chnaged function

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,11 @@
 var apiKey = "&appid=dbaec924e284909f3d31bc2652b4488f&units=imperial";
 var currentUrl = "https://api.openweathermap.org/data/2.5/weather?q="; //q = query
 var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=";
-var uvUrl =
-  "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}";
+var uvUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=";
 var citySearch = $("#searchCity");
-var currentTemp = $("#Temperature");
+var cityName = $(".cityName");
+var weatherImg = $(".weather");
+var currentTemp = $("#temp");
 var currentWind = $("#wind");
 var currentHumidity = $("#humidity");
 var uvI = $("#uvI");
@@ -23,6 +24,16 @@ function currentWeather(city) {
     dataType: "json",
     success: function (data) {
       console.log(data);
+      cityName.empty();
+      cityName.append(data.name + " (" + new Date().toLocaleDateString() + ")");
+      var src = weatherImg.append();
+      currentTemp.empty();
+      currentTemp.append(data.main.temp + " °F");
+      currentHumidity.empty();
+      currentHumidity.append(data.main.humidity + "%");
+      currentWind.empty();
+      currentWind.append(data.wind.speed + " MPH");
+      getUv(data.coord.lat, data.coord.lon);
     },
   });
 }
@@ -31,7 +42,18 @@ function currentWeather(city) {
 
 // currentDay (city) {data.main.temp}
 
-//funciton getUv (lat,long) {}
+function getUv(lat, lon) {
+  $.ajax({
+    type: "GET",
+    url: uvUrl + lat + "&lon=" + lon + apiKey,
+    dataType: "json",
+    success: function (data) {
+      uvI.empty();
+      uvI.append(data.value);
+      console.log(data);
+    },
+  });
+}
 
 //data.coord.lat inside function
 //data.coord.lon
